197484c287c36c7c3a3646b0dc1b339e5c09c959
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2025-01-27 10:53:54 -0500

    Merge origin/main into main
    # Conflicts:
    #	jettraserverhelloworld/src/main/java/com/avbravo/jettraserverhelloworld/start/Start.java

--- jettraserverhelloworld/src/main/java/com/avbravo/jettraserverhelloworld/view/JmoordbJ2htmlView.java
+++ jettraserverhelloworld/src/main/java/com/avbravo/jettraserverhelloworld/view/JmoordbJ2htmlView.java
@@ -25,19 +25,31 @@ public class JmoordbJ2htmlView implements JettraView {
 
     @Override
     public Response draw() {
-        List<Country> result = countryRepository.findAll();
-        var bod = body(
-                div(attrs("#country"),
-                        p("Some sibling element"),
-                        each(filter(result, country -> country != null), country
-                                -> div(attrs(".country"),
-                                h2(country.getIdcountry()),
-                                p(country.getCountry())
+        var response = "";
+        try {
+            List<Country> result = countryRepository.findAll();
+
+            if (result == null || result.isEmpty()) {
+                response = body(h1("No se obtuvieron datos")).render();
+            } else {
+                response = body(
+                        div(attrs("#country"),
+                                p("Some sibling element"),
+                                each(filter(result, country -> country != null), country
+                                        -> div(attrs(".country"),
+                                        h2(country.getIdcountry()),
+                                        p(country.getCountry())
+                                )
+                                )
                         )
-                        )
-                )
-        ).render();
-        return Response.ok(bod).build();
+                ).render();
+            }
+
+        } catch (Exception e) {
+            response = body(h1("No se obtuvieron datos")).render();
+        }
+
+        return Response.ok(response).build();
     }
 //  
 }
